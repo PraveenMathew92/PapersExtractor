d0026ca4f7556bbef1c409e741cb50f81812b874
Praveen Mathew <praveen.mathew92@outlook.com>
2021-02-07 19:06:57 +0530

    Include refextractor

--- extractor.py
+++ extractor.py
@@ -1,6 +1,6 @@
 import os
 from PyPaperBot import __main__ as bot
-from inspect import getmembers, isfunction
+from refextract import extract_references_from_file
 
 query = "Machine Learning"
 default_scholar_pages = 1
@@ -11,6 +11,8 @@ os.makedirs(dwn_dir, exist_ok=True)
 # Download Papers
 # bot.start(query, default_scholar_pages, dwn_dir)
 
-for _, _, papers in (os.walk(dwn_dir)):
+for dirpath, _, papers in (os.walk(dwn_dir)):
     for paper in papers:
-        print(paper)
+        paper_path = os.path.join(dirpath, paper)
+        print(paper_path)
+        print(extract_references_from_file(paper_path))
