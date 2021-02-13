1d38c6ea23497933a5ce4147a21da1cc012f9b0d
Praveen Mathew <praveen.mathew92@outlook.com>
2021-02-14 00:13:59 +0530

    Get details from doi

--- extractor.py
+++ extractor.py
@@ -1,18 +1,28 @@
 import os
 from PyPaperBot import __main__ as bot
-from refextract import extract_references_from_file
+import json
+import pandas
+from crossref.restful import Works
 
 query = "Machine Learning"
 default_scholar_pages = 1
-dwn_dir = os.getcwd() + "/downloads"
+dwn_dir = os.getcwd() + "/downloads/"
 
 os.makedirs(dwn_dir, exist_ok=True)
 
 # Download Papers
 # bot.start(query, default_scholar_pages, dwn_dir)
 
-for dirpath, _, papers in (os.walk(dwn_dir)):
-    for paper in papers:
-        paper_path = os.path.join(dirpath, paper)
-        print(paper_path)
-        print(extract_references_from_file(paper_path))
+works = Works()
+results_csv = pandas.read_csv('result.csv')
+dois = results_csv['DOI']
+
+for doi in dois:
+    details = works.doi(doi)
+    print(json.dumps(details))
+
+# for dirpath, _, papers in (os.walk(dwn_dir)):
+#     for paper in papers:
+#         paper_path = os.path.join(dirpath, paper)
+#         print(paper_path)
+        # print(extract_references_from_file(paper_path))
