44476c1fbd8d7c4f04bf1d604eb80f42131ebd4c
Praveen Mathew <praveen.mathew92@outlook.com>
2021-02-28 19:43:07 +0530

    Get abstract

--- extractor.py
+++ extractor.py
@@ -4,6 +4,20 @@ import json
 import pandas
 from crossref.restful import Works
 
+import rpy2
+import rpy2.robjects.packages as rpackages
+from rpy2.robjects.vectors import StrVector
+from rpy2.robjects.packages import importr
+utils = rpackages.importr('utils')
+utils.chooseCRANmirror(ind=1)
+
+# Install packages
+packnames = ('rcrossref')
+utils.install_packages(StrVector(packnames))
+
+# Load packages
+rcrossref = importr('rcrossref')
+
 query = "Machine Learning"
 default_scholar_pages = 1
 dwn_dir = os.getcwd() + "/downloads/"
@@ -20,6 +34,7 @@ dois = results_csv['DOI']
 for doi in dois:
     details = works.doi(doi)
     print(json.dumps(details))
+    print(rcrossref.cr_abstract(doi))
 
 # for dirpath, _, papers in (os.walk(dwn_dir)):
 #     for paper in papers:
